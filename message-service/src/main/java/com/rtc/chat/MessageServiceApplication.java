8a5de6865bfd44b0eabd6cf2e9aad13df7426a22
Nikhil Kaushal <kaushalnikhil99@gmail.com>
2023-07-18 21:39:44 +0530

    refactor: Rename chat-service to message-service

--- message-service/src/main/java/com/rtc/chat/MessageServiceApplication.java
+++ message-service/src/main/java/com/rtc/chat/MessageServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatServiceApplication {
+public class MessageServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ChatServiceApplication.class, args);
+    SpringApplication.run(MessageServiceApplication.class, args);
   }
 
 }
